39450c2f1dab76cf429543f9384c23a95a9c8a7e
rom4es <rom4es_1993@mail.ru>
2018-02-27 22:51:48 +0700

    123

--- js/main.js
+++ js/main.js
@@ -190,12 +190,12 @@ $(document).ready(function(){
         slideoutLeft.open();
         $('.basket-menu').addClass("hide");
         $('.mobile-menu').removeClass("hide");
-        $('.b-menu-overlay').show();slideout.enableTouch();
+        $('.b-menu-overlay').show();
         return false;
     });
     $('.b-menu-overlay').click(function() {
         slideoutLeft.close();
-        $('.b-menu-overlay').hide();slideout.disableTouch();
+        $('.b-menu-overlay').hide();
         return false;
     });
 
